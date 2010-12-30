b08c90c1fd7096276a2beaa9a09251f178cfc559
mario.volarevic@gmail.com <mario.volarevic@gmail.com@7e7ab4ed-0c63-ca0b-fbce-1cd8418a3511>
2010-12-30 10:40:30 +0000

    - promijenjen kod zastitnog kodera, sada se posljednjih nekoliko bitova koji nisu djeljivi sa 7 odbacuje, tako je rekao Mikac, slao sam mu mail

--- trunk/zast_koder_par/zas_k_par.cpp
+++ trunk/zast_koder_par/zas_k_par.cpp
@@ -7,16 +7,16 @@ void paritetni_koder() //dodaje paritetni bit na kraj
 	int brb = 0, brbit = 0;
 	FILE *pFileR = fopen("23.txt","r");
 	FILE *pFileW = fopen("34.txt","w");
-	while (!feof(pFileR))
+	while (1)
 	{
 		int brj = 0;
 		char blok[9] = {""};	//neka privremeno velicina bloka bude 8, to je 7 podatkovnih i 1 zastitni, s tim da je zadnji velicine onoliko koliko ostane
 		fscanf(pFileR,"%7c",blok);
+		if (feof(pFileR)) break;
 		for (int i = 0; i < strlen(blok); i++)
 		{
 			if (blok[i] == '1') brj++;
 		}
-		//printf("%d",brj);
 		if (brj % 2 == 0) strcat(blok,"0");
 		else strcat(blok,"1");
 		fprintf(pFileW,"%s",blok);
@@ -25,7 +25,7 @@ void paritetni_koder() //dodaje paritetni bit na kraj
 	}
 	fclose(pFileR);
 	fclose(pFileW);
-	printf("\nGenerirano %d blokova pomocu %d bitova\n",brb,brbit);
+	printf("\nGenerirano %d 8-bitnih blokova pomocu %d bitova\n",brb,brbit);
 }
 
 
