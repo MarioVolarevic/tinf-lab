000ca6179e31ef22f9133041eb2afcd91adf2702
mario.volarevic@gmail.com <mario.volarevic@gmail.com@7e7ab4ed-0c63-ca0b-fbce-1cd8418a3511>
2011-01-06 11:55:19 +0000

    - jos par izmjena da se moze napravit zadatak u izvjecsu pod 5. i 6. i da se lakse izracuna ovo ostalo, jer sad vraca broj znakova - i dodao sam teh_dokumentaciju sto je alena napravila

--- TINF_lab/Release/runall.bat
+++ TINF_lab/Release/runall.bat
@@ -2,8 +2,8 @@
 izvor.exe
 ent_kod.exe 12.txt
 z_kod.exe 23.txt
-gen_vek_pogreske.exe -s
+gen_vek_pogreske.exe 34.txt -s
 kanal.exe 34.txt err_vec.txt
 z_dekod.exe 45.txt
 ent_dekod.exe 56.txt
-rem pause
+pause
